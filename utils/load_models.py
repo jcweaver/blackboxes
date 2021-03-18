c3e6ec2fcc4c0a504e92de76c359eaa7db9def17
mspuckit <mspuckit@hotmail.com>
2021-03-18 01:52:14 +0000

    new files not yet tested

---
+++ utils/load_models.py
@@ -0,0 +1,35 @@
+#%%
+############################################################################
+# IMPORTS
+############################################################################
+
+import pandas as pd
+import numpy as np
+import transform_data
+
+import argparse
+import pickle
+import os
+
+
+#%%
+
+
+
+def train_model_lenet5(train, verbose = True):
+
+    #split the data
+
+    #create a model and return it? or pickle it? 
+    model = []
+    return model
+
+
+# %%
+
+if __name__ == "__main__":
+        
+    
+    print("hello")
+# %%
+
