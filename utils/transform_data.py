3360786f2987a028f303eb5065cdd92a1f00ac02
Jackie Nichols <mspuckit@hotmail.com>
2021-03-30 18:59:14 -0700

    added bright and dim

--- utils/transform_data.py
+++ utils/transform_data.py
@@ -16,20 +16,7 @@ class TransformData(object):
     def __init__(self, scale = 255.0, reshape = 96, verbose = False, prop = 0.1):
         
         # validate that the constructor parameters were provided by caller
-        #I don't think we need the pickle path in here...but keep this code because 
-        #I'll want to reuse this elsewhere. 
-        #if (not pickle_file_path):
-        #    raise RuntimeError('Please provide a path to the pickle files.')
         
-        #Get a clean path...didn't work for me on first try because of spaces. 
-        #pickle_file_path = str(pickle_file_path).replace('\\', '/').strip()
-        #if (not pickle_file_path.endswith('/')): pickle_file_path = ''.join((pickle_file_path, '/'))
-
-        #Let's make sure all is good in the world and we can find the path
-        #if (not os.path.isdir(pickle_file_path)):
-        #    raise RuntimeError("Path of pickle files '%s' is invalid. Please resolve and try again." % pickle_file_path)
-
-        #self.__pickle_file_path = pickle_file_path #Do we need this? not sure but for now let's keep it. 
         self.__scale_image_by = scale
         self.__reshape_image = reshape
         
@@ -48,27 +35,6 @@ class TransformData(object):
         else:
             coordinates = [c for c in df.columns if c.endswith('_y')]
         return coordinates
-
-    #it might be easier to work wtih a dictionary as these are pairs??
-    def __get_coordinate_dict(self):
-        coord = {
-                'left_eye_center_x': 'left_eye_center_y',  
-                'right_eye_center_x': 'right_eye_center_y', 
-                'left_eye_inner_corner_x': 'left_eye_inner_corner_y', 
-                'left_eye_outer_corner_x': 'left_eye_outer_corner_y', 
-                'right_eye_inner_corner_x': 'right_eye_inner_corner_y', 
-                'right_eye_outer_corner_x': 'right_eye_outer_corner_y', 
-                'left_eyebrow_inner_end_x': 'left_eyebrow_inner_end_y', 
-                'left_eyebrow_outer_end_x': 'left_eyebrow_outer_end_y', 
-                'right_eyebrow_inner_end_x': 'right_eyebrow_inner_end_y', 
-                'right_eyebrow_outer_end_x': 'right_eyebrow_outer_end_y', 
-                'nose_tip_x': 'nose_tip_y', 
-                'mouth_left_corner_x': 'mouth_left_corner_y', 
-                'mouth_right_corner_x': 'mouth_right_corner_y', 
-                'mouth_center_top_lip_x': 'mouth_center_top_lip_y', 
-                'mouth_center_bottom_lip_x': 'mouth_center_bottom_lip_y'}
-
-        return coord
 
     #it might be easier to work wtih a dictionary as these are pairs??
     def __get_coordinate_dict_flipped(self):
@@ -103,14 +69,15 @@ class TransformData(object):
     
     ##### PUBLIC
     ###########################################
-    ##  Flip Horizonal
+    #  Flip Horizonal
+    #  Inspired from
+    # https://www.kaggle.com/balraj98/data-augmentation-for-facial-keypoint-detection
+    #
     ###########################################   
-    #NOT TESTED
     def FlipHorizontal(self, train, verbose = False):
         #Flip the images horizontaly and adjust the labels. 
         
         adj_train = train.copy()
-        
         
         #this will only do all the keypoints. Not sure about this..
         #removes null rows
@@ -119,7 +86,7 @@ class TransformData(object):
         #https://www.techbeamers.com/python-map-function/ with lambda 
         # horizontally flip the images - use the map function
         adj_train.image = adj_train.image.map(lambda x: np.flip(x.reshape(self.__reshape_image,self.__reshape_image), axis=1).ravel() )
-        
+
         cols = self.__get_coordinate_columns(adj_train, True, False)
         
         # shift all 'x' values by linear mirroring
@@ -143,72 +110,76 @@ class TransformData(object):
             print(f"New Horizontal shape: {adj_train.shape}")
         return adj_train
 
-
+    #
     ###########################################
-    ##  Bright and Dim
-    ###########################################
-    #https://scipy-lectures.org/advanced/image_processing/
-    #https://www.tutorialspoint.com/scipy/scipy_ndimage.htm
-
-    def Bright_Dim(self, train, level_of_brightness = 1.2, level_to_dim = 0.6, verbose = False):
+    #  Bright and Dim
+    #  Inspired from
+    #  https://www.kaggle.com/balraj98/data-augmentation-for-facial-keypoint-detection
+    #
+    #  https://scipy-lectures.org/advanced/image_processing/
+    #  https://www.tutorialspoint.com/scipy/scipy_ndimage.htm
+    def Bright_Dim(self, train, level_of_brightness = 1, level_to_dim = 1, verbose = False):
         
         #Used to brighten
         bright_train = train.copy()
 
-        # Get the items with all 15 points
-        bright_train = bright_train[(bright_train.isnull().sum(axis = 1) == 0)]
-
         # Used for dimming
         dim_train = bright_train.copy()
 
-        if verbose: 
-            print(f"Number of images to be brightened: {bright_train.shape[0]}")
-        #https://www.techbeamers.com/python-map-function/ with lambda 
-        #Apply a level of brightness with min =0 and max = 1 for every image in bright_train
-        bright_train.image = bright_train.image.map(lambda x: np.clip(x * level_of_brightness, 0.0, 1.0))
+        if level_of_brightness == 1:
+            print("Skipping brightness")
+        else:
+            if verbose: 
+                print(f"Number of images to be brightened: {bright_train.shape[0]}")
+            #https://www.techbeamers.com/python-map-function/ with lambda 
+            #Apply a level of brightness with min =0 and max = 1 for every image in bright_train
+            bright_train.image = bright_train.image.map(lambda x: np.clip(x * level_of_brightness, 0.0, 1.0))
         
-        if verbose: 
-            print(f"Number of images to be dimmed: {dim_train.shape[0]}")
-        #https://www.techbeamers.com/python-map-function/ with lambda 
-        #Apply a level of dim with min =0 and max = 1 for every image in dim_train    
-        dim_train.image = dim_train.image.map(lambda x: np.clip(x * level_to_dim, 0.0, 1.0))
+        
+        if level_to_dim == 1:
+            print("Skipping dim")
+        else:
+            if verbose: 
+                print(f"Number of images to be dimmed: {dim_train.shape[0]}")
+            #https://www.techbeamers.com/python-map-function/ with lambda 
+            #Apply a level of dim with min =0 and max = 1 for every image in dim_train    
+            dim_train.image = dim_train.image.map(lambda x: np.clip(x * level_to_dim, 0.0, 1.0))
+            #Append dimmed images to brightened images. 
+            bright_train = bright_train.append(dim_train, ignore_index = True).reset_index().drop(columns=['index'])
 
-        #Append dimmed images to brightened images. 
-        aug_train = bright_train.append(dim_train, ignore_index = True).reset_index().drop(columns=['index'])
-
         if verbose: 
-            print(f"Completed brighten and dim. Number of observations added to train: {aug_train.shape[0]}")
-        return aug_train
+            print(f"Completed brighten and dim. Number of observations added to train: {bright_train.shape[0]}")
+        return bright_train   
+
 
     ###########################################
-    ##  Scale Images
+    #  Scale Images
+    #  Inspired by
+    #  https://www.kaggle.com/balraj98/data-augmentation-for-facial-keypoint-detection#Exploring-Data
     ###########################################
     def ScaleImages(self, df, verbose = False):
         #For most CNN we will need to scale the images by 255. 
-
         if verbose: 
-            print("Scaling %d images..." % df.shape[0])
+            print("Scaling images")
         if 'image' in df.columns:
-            
             image_scaled = df.image.values
-            #print("Before:",image_scaled)
             image_scaled = image_scaled / self.__scale_image_by
             df.image = image_scaled
-            #print("after:",image_scaled)
-
             if verbose: 
-                print("Scaling of %d observations complete." % df.shape[0])
+                print("Scaling complete.")
         else:
             print("Scaling not complete. Column 'image' missing.")
         
-        #Return the df. Note: user should pickle after this. 
+        #Return the df. Note: user may want to pickle this. 
         return df
 
 
     ###########################################
-    ##  Split Train
+    #  Split Train
+    #
+    #
     ###########################################
-    def SplitTrain(self, train, normalize = True, verbose = True):
+    def SplitTrain(self, train, verbose = True):
         #Split the train data into X,Y and in the proper shape
         #Everyone should be able to use this
 
@@ -229,39 +200,33 @@ class TransformData(object):
         X = np.array(X)
 
         Y = temp_df.drop(columns=['image']).values
-        #Y = temp_df[temp_df.columns[1:-1]].values
+        
+        for i in range(Y.shape[1]):
+            new_arr = []
+            current_arr = Y[:,i]
+            for j in range(len(current_arr)):
+                new_arr.append((current_arr[j] - 48.)/48.)    
+            Y[:,i] = new_arr
 
-        #This has already been normalized but this will give us -1,1? 
-        if normalize:
-            #Since the data has already been normalized not sure we need to do this
-            #this is to test it out. Yes we do need to do this!!
-            for i in range(Y.shape[1]):
-                new_arr = []
-                current_arr = Y[:,i]
-                for j in range(len(current_arr)):
-                    new_arr.append((current_arr[j] - 48.)/48.)    
-                Y[:,i] = new_arr
         return X, Y
         
     ###########################################
-    ##  Split Test
+    #  Split Test
+    #
+    #
     ###########################################
     def SplitTest(self, test, ids, verbose = False):
         
         if verbose:
             print("Begining the split of Test")
 
-        
         id_lookup = ids
     
-
         #Get the unique image id's. 
         unique_ids = id_lookup.image_id.unique()
-        print("got unique ids")
-
+        
         #Get the subset of images in test
         test_subset = test[(test.image_id.isin(unique_ids))]
-        print("test subset shape:", test_subset.shape)
 
         #reshape the images
         test_subset.image = test_subset.image.map(lambda x: np.array(x).reshape(96,96,1))     
